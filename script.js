7112eb2b88f25a857dd1874a388f8186a790d4a0
zephyr2101 <159362566+zephyr2101@users.noreply.github.com>
2024-09-21 17:18:26 -0700

    very cool project=finished

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    window.location = 'https://www.youtube.com/watch?v=oHg5SJYRHA0'
+})
